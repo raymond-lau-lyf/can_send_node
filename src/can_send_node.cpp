15d00bb238434f5d61df858b380e3893138c277d
raymond <2583701681@qq.com>
2023-03-06 23:59:19 +0800

    add imu send test

--- src/can_send_node.cpp
+++ src/can_send_node.cpp
@@ -2,6 +2,15 @@
 #include "can_msgs/Frame.h"
 #include "sensor_msgs/Imu.h"
 
+void Imu_msg_update(sensor_msgs::Imu *imu_msg_p){
+    imu_msg_p->header.stamp=ros::Time::now();
+    imu_msg_p->header.frame_id="imu";
+    imu_msg_p->orientation.w=-0.5;
+    imu_msg_p->orientation.x=0.5;
+    imu_msg_p->orientation.y=0.5;
+    imu_msg_p->orientation.z=0.5;
+}
+
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL,"");
@@ -10,26 +19,43 @@ int main(int argc, char *argv[])
     ros::Publisher pub=nh.advertise<sensor_msgs::Imu>("pub_imu_data",10);
 
     sensor_msgs::Imu imu_msg;
-    // learning_vehicle::Frame t_message;
-    // t_message.id=161;
-    // t_message.dlc=8;
-    // t_message.data[0]=1;
-    // t_message.data[1]=0;
-    // t_message.data[2]=0;
-    // t_message.data[3]=0;
-    // t_message.data[4]=0;
-    // t_message.data[5]=0;
-    // t_message.data[6]=0;
-    // t_message.data[7]=0;
-
-    ros::Rate rate(10);
+    imu_msg.header.seq=0;
+    imu_msg.header.stamp=ros::Time::now();
+    imu_msg.header.frame_id="imu";
+    imu_msg.orientation.w=-0.5;
+    imu_msg.orientation.x=0.5;
+    imu_msg.orientation.y=0.5;
+    imu_msg.orientation.z=0.5;
+    ros::Rate rate(500);
     while (ros::ok())
     {
         ROS_INFO("发送成功！");
+        Imu_msg_update(&imu_msg);
         pub.publish(imu_msg);
         rate.sleep();
         ros::spinOnce();
     }
-    
+
     return 0;
 }
+
+// std_msgs/Header header
+//   uint32 seq  
+//   time stamp   // 时间戳
+//   string frame_id   
+// geometry_msgs/Quaternion orientation   // 姿态
+//   float64 x
+//   float64 y
+//   float64 z
+//   float64 w
+// float64[9] orientation_covariance   // 姿态协方差
+// geometry_msgs/Vector3 angular_velocity   // 角速度
+//   float64 x
+//   float64 y
+//   float64 z
+// float64[9] angular_velocity_covariance   // 角速度协方差
+// geometry_msgs/Vector3 linear_acceleration  // 线加速度
+//   float64 x
+//   float64 y
+//   float64 z
+// float64[9] linear_acceleration_covariance   // 线加速度协方差
