01b14e62d6dc444fdaa965a4631ef8b58b64577b
raymond <2583701681@qq.com>
2023-03-07 02:44:05 +0800

    [add func] quaternion to euler

--- src/can_send_node.cpp
+++ src/can_send_node.cpp
@@ -1,39 +1,45 @@
 #include "can_send_node/can_send_node.h"
 
-bool data_updated=false;
+unsigned char data_updated = false;
+sensor_msgs::Imu imu_msg;
+float roll, pitch, yaw;
+
+static void toEulerAngle(const double x, const double y, const double z, const double w) {
+    // roll (x-axis rotation)
+    double sinr_cosp = +2.0 * (w * x + y * z);
+    double cosr_cosp = +1.0 - 2.0 * (x * x + y * y);
+    roll = atan2(sinr_cosp, cosr_cosp);
+
+    // pitch (y-axis rotation)
+    double sinp = +2.0 * (w * y - z * x);
+    if (fabs(sinp) >= 1)
+        pitch = copysign(M_PI / 2, sinp);  // use 90 degrees if out of range
+    else
+        pitch = asin(sinp);
+
+    // yaw (z-axis rotation)
+    double siny_cosp = +2.0 * (w * z + x * y);
+    double cosy_cosp = +1.0 - 2.0 * (y * y + z * z);
+    yaw = atan2(siny_cosp, cosy_cosp);
+    //    return yaw;
+}
 
 void Imu_msg_update(can_msgs::Frame *send_msg_p);
-void ImuCallback(const sensor_msgs::ImuConstPtr &imu_msg){
-    data_updated=true;
+void ImuCallback(const sensor_msgs::ImuConstPtr &imu_msg) {
+    toEulerAngle(imu_msg->orientation.x, imu_msg->orientation.y, imu_msg->orientation.z, imu_msg->orientation.w);
+    data_updated = true;
 }
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL, "");
     ros::init(argc, argv, "can_send");
     ros::NodeHandle nh;
 
-    ros::Subscriber sub = nh.subscribe<sensor_msgs::Imu>("/sub_imu_data", 10,ImuCallback);
+    ros::Subscriber sub = nh.subscribe<sensor_msgs::Imu>("/imu/data_raw", 10, ImuCallback);
     ros::Publisher pub_imu = nh.advertise<can_msgs::Frame>("/pub_can_imu_data", 10);
 
-    sensor_msgs::Imu imu_msg;
-    imu_msg.header.seq = 0;
-    imu_msg.header.stamp = ros::Time::now();
-    imu_msg.header.frame_id = "imu";
-    imu_msg.orientation.w = -0.5;
-    imu_msg.orientation.x = 0.5;
-    imu_msg.orientation.y = 0.5;
-    imu_msg.orientation.z = 0.5;
-
     can_msgs::Frame can_imu_msg;
-    can_imu_msg.id= 0x001;
-    can_imu_msg.dlc=8;
-    can_imu_msg.data[0]=1;
-    can_imu_msg.data[1]=0;
-    can_imu_msg.data[2]=1;
-    can_imu_msg.data[3]=0;
-    can_imu_msg.data[4]=1;
-    can_imu_msg.data[5]=0;
-    can_imu_msg.data[6]=1;
-    can_imu_msg.data[7]=0;
+    can_imu_msg.id = 0x001;
+    can_imu_msg.dlc = 8;
 
     ros::Rate rate(200);
     while (ros::ok()) {
@@ -47,16 +53,35 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+bool start = true;
+unsigned char c[sizeof(float)];
 void Imu_msg_update(can_msgs::Frame *send_msg_p) {
-    send_msg_p->data[0]=1;
-    send_msg_p->data[1]=0;
-    send_msg_p->data[2]=1;
-    send_msg_p->data[3]=0;
-    send_msg_p->data[4]=1;
-    send_msg_p->data[5]=0;
-    send_msg_p->data[6]=1;
-    send_msg_p->data[7]=0;
-    data_updated=false;
+    if (start) {
+        send_msg_p->data[0] = 's';
+        memcpy(c, &roll, sizeof(roll));
+        send_msg_p->data[1] = c[0];
+        send_msg_p->data[2] = c[1];
+        send_msg_p->data[3] = c[2];
+        send_msg_p->data[4] = c[3];
+        memcpy(c, &pitch, sizeof(pitch));
+        send_msg_p->data[5] = c[0];
+        send_msg_p->data[6] = c[1];
+        send_msg_p->data[7] = c[2];
+        start = false;
+    } else {
+        memcpy(c, &pitch, sizeof(pitch));
+        send_msg_p->data[0] = c[3];
+        memcpy(c, &yaw, sizeof(yaw));
+        send_msg_p->data[1] = c[0];
+        send_msg_p->data[2] = c[1];
+        send_msg_p->data[3] = c[2];
+        send_msg_p->data[4] = c[3];
+        send_msg_p->data[5] = data_updated;
+        send_msg_p->data[6] = 'e';
+        start = true;
+    }
+
+    data_updated = false;  // set upd flag to false
 }
 
 // std_msgs/Header header
